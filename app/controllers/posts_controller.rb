54108533a97d1d5a91debaf5c25a780883d1d7ca
jhon11390 <meljhon2@gmail.com>
2020-05-22 05:39:25 -0500

    Configura controlador para que se vea index y show al salir de session

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,10 @@
 class PostsController < ApplicationController
+    before_action :authenticate_user!, except: [:index, :show]
 
     def index
         @posts = Post.all
     end
-
+    
     def show
         @post = Post.find(params[:id])
     end
