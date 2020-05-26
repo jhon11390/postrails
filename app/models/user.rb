c6da30582866b9ee4396b80974c47817ce0c9729
jhon11390 <meljhon2@gmail.com>
2020-05-26 17:59:50 -0500

    Arregla problema de registro editando el modelo User

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,4 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
-  validates :email, :password, presence: true
-  validates :email, :password, uniqueness: true
 end
